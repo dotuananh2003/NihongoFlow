614a374d4985fa5f1cc4e3c78bb71c783132248c
Developer <developer@example.com>
2026-07-23 11:07:33 +0700

    feat: implement 90fps zero-momentum smooth scrolling with Lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ReactLenis } from 'lenis/react';
 import { AppRoutes } from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.8,
+        duration: 1.2,
+        smoothWheel: true,
+        syncTouch: true,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+      }}
+    >
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ReactLenis>
   );
 }
 
