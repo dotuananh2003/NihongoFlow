1b64f0d0bd4e772dc260295a1e550c79ae7f3c62
Developer <developer@example.com>
2026-07-23 11:12:23 +0700

    chore: remove lenis and revert to native smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
-import { ReactLenis } from 'lenis/react';
 import { AppRoutes } from './routes';
 
 function App() {
   return (
-    <ReactLenis
-      root
-      options={{
-        duration: 0.15,
-        wheelMultiplier: 1.8,
-        smoothWheel: true,
-        syncTouch: true,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
-      }}
-    >
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </ReactLenis>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
