d04df79ad2de1cfa212c163211f0cb344348bc07
Developer <developer@example.com>
2026-08-29 12:55:17 +0700

    Fix scroll delay by removing custom nativeSmoothScroll logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { AuthProvider } from './context/AuthContext';
-import { initNativeSmoothScroll } from './utils/nativeSmoothScroll';
 
 function App() {
-  useEffect(() => {
-    const smoothScroll = initNativeSmoothScroll();
-    return () => {
-      smoothScroll?.destroy();
-    };
-  }, []);
-
   return (
     <BrowserRouter>
       <AuthProvider>
